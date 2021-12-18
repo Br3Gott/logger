5d65902224844d65e263dc7a0dd51748bd68ffac
davso188 <davso188@student.liu.se>
2021-12-18 22:29:27 +0100

    Added error handling to file write

--- app.js
+++ app.js
@@ -21,14 +21,18 @@ let local_data = JSON.parse(fs.readFileSync("db.json"));
 
 //Save data to json file every 10min.
 setInterval(() => {
-  fs.writeFile("db.json", local_data);
+  fs.writeFile("db.json", local_data, (err) => {
+    if (err) throw err;
+  });
 }, 600000);
 
 const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const { Server } = require("socket.io");
+const {
+  Server
+} = require("socket.io");
 const io = new Server(server);
 const port = 3000;
 
@@ -52,27 +56,40 @@ let path = "/home/dawe/web_dev/logs/access.log";
 
 fs.watchFile(path, (eventType, filename) => {
 
-    readLastLines.read(path, 1)
-        .then((line) => {
-            let split = line.split(":.:");
-            local_data.host.push(split[1]);
-            local_data.user_agent.push(split[2]);
-            local_data.protocol.push(split[3]);
-            local_data.resource.push(split[4]);
-            console.log(split[0]);
-            console.log({host: split[1], user_agent: split[2], protocol: split[3], resource: split[4],});
-            io.emit("log", {host: split[1], user_agent: split[2], protocol: split[3], resource: split[4],});
+  readLastLines.read(path, 1)
+    .then((line) => {
+      let split = line.split(":.:");
+      local_data.host.push(split[1]);
+      local_data.user_agent.push(split[2]);
+      local_data.protocol.push(split[3]);
+      local_data.resource.push(split[4]);
+      console.log(split[0]);
+      console.log({
+        host: split[1],
+        user_agent: split[2],
+        protocol: split[3],
+        resource: split[4],
+      });
+      io.emit("log", {
+        host: split[1],
+        user_agent: split[2],
+        protocol: split[3],
+        resource: split[4],
+      });
 
-            fetch('http://ip-api.com/json/' + split[0])
-                .then(response => response.json())
-                .then(data => {
-                    if(data.lat != null) {
-                        local_data.coords.push(data);
-                        io.emit("coords", {"lat": (parseFloat(data.lat)), "lon": (parseFloat(data.lon))});
-                        io.emit("geoip", data);
-                    }
-                })
-                .catch(err => console.error(err));
-        });
-    
+      fetch('http://ip-api.com/json/' + split[0])
+        .then(response => response.json())
+        .then(data => {
+          if (data.lat != null) {
+            local_data.coords.push(data);
+            io.emit("coords", {
+              "lat": (parseFloat(data.lat)),
+              "lon": (parseFloat(data.lon))
+            });
+            io.emit("geoip", data);
+          }
+        })
+        .catch(err => console.error(err));
+    });
+
 });
